4d1aed689f29b2ffb5cacdba51ad640feed6c7c8
Nick Lombardi <nelom3@gmail.com>
2021-06-16 21:49:56 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connections (
+ActiveRecord::Base.establish_connection (
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
   )
